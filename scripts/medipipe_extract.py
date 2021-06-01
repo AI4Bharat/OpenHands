7eb472ba2e82e7e5b978e82152f2cc3c348de08b
Prem <sprem1997@gmail.com>
2021-06-01 17:28:01 +0530

    small changes

--- scripts/medipipe_extract.py
+++ scripts/medipipe_extract.py
@@ -3,14 +3,17 @@ import os
 import numpy as np
 import mediapipe as mp
 from tqdm.auto import tqdm
-import matplotlib.pyplot as plt
 import multiprocessing
 from joblib import Parallel, delayed
 
 mp_holistic = mp.solutions.holistic
 
+N_FACE_LANDMARKS = 468
+N_BODY_LANDMARKS = 33
+N_HAND_LANDMARKS = 21
 
-def process_pose_landmarks(component, width, height, n_points):
+
+def process_body_landmarks(component, width, height, n_points):
     kps = np.zeros((n_points, 3))
     conf = np.zeros(n_points)
     if component is not None:
@@ -31,10 +34,6 @@ def process_other_landmarks(component, width, height, n_points):
 
 
 def get_holistic_keypoints(frames, width, height):
-    N_FACE_LANDMARKS = 468
-    N_BODY_LANDMARKS = 33
-    N_HAND_LANDMARKS = 21
-
     holistic = mp_holistic.Holistic(static_image_mode=False)
 
     keypoints = []
@@ -43,7 +42,7 @@ def get_holistic_keypoints(frames, width, height):
     for i, frame in enumerate(tqdm(frames)):
         results = holistic.process(frame)
 
-        body_data, body_conf = process_pose_landmarks(
+        body_data, body_conf = process_body_landmarks(
             results.pose_landmarks, width, height, N_BODY_LANDMARKS
         )
         face_data, face_conf = process_other_landmarks(
@@ -64,7 +63,6 @@ def get_holistic_keypoints(frames, width, height):
 
     keypoints = np.stack(keypoints)
     confs = np.stack(confs)
-
     return keypoints, confs
 
 
@@ -81,22 +79,30 @@ def load_frames_from_video(video_path):
     return np.asarray(frames)
 
 
-def gen_keypoints_for_video(video_path):
+def gen_keypoints_for_video(video_path, save_path):
     frames = load_frames_from_video(video_path)
     width, height, _ = frames[0].shape
     kps, confs = get_holistic_keypoints(frames, width, height)
-
-    # TODO: Save
-    return {"kps": kps, "confs": confs}
+    confs = np.expand_dims(confs, axis=-1)
+    data = np.concatenate([kps, confs], axis=-1)
+    np.save(save_path, data)
 
 
 n_cores = multiprocessing.cpu_count()
-file_paths = []
+
 DIR = "AUTSL/train/"
+SAVE_DIR = "AUTSL/holistic_poses/"
+
+os.makedirs(SAVE_DIR, exist_ok=True)
+
+file_paths = []
+save_paths = []
 for file in os.listdir(DIR):
     if "color" in file:
         file_paths.append(os.path.join(DIR, file))
+        save_paths.append(os.path.join(SAVE_DIR, file.replace(".mp4", "")))
 
 Parallel(n_jobs=n_cores, backend="multiprocessing")(
-    delayed(gen_keypoints_for_video)(path) for path in tqdm(file_paths)
+    delayed(gen_keypoints_for_video)(path, save_path)
+    for path, save_path in tqdm(zip(file_paths, save_paths))
 )
