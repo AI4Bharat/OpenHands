2f26455c7cb530265618949203859b953224d0aa
Prem <sprem1997@gmail.com>
2021-06-17 22:02:27 +0530

    Bug fix on Smooth CE

--- slr/core/losses.py
+++ slr/core/losses.py
@@ -7,6 +7,7 @@ CrossEntropyLoss = nn.CrossEntropyLoss
 
 class SmoothedCrossEntropyLoss(nn.Module):
     def __init__(self, smooth_factor=0.1):
+        super(SmoothedCrossEntropyLoss, self).__init__()
         self.smooth_factor = smooth_factor
 
     def forward(self, input, target):
@@ -14,4 +15,4 @@ class SmoothedCrossEntropyLoss(nn.Module):
         nll_loss = -log_probs.gather(dim=-1, index=target.unsqueeze(1)).squeeze(1)
         smooth_loss = -log_probs.mean(dim=-1)
         loss = (1.0 - self.smooth_factor) * nll_loss + self.smooth_factor * smooth_loss
-        return loss
+        return loss.mean()
