8d52f7423340fd3123c1f159fee816853bec5011
Ubuntu <manideep@manideep-training.tsk1eagr2o4unbgmelpbqu45rb.jx.internal.cloudapp.net>
2022-02-28 18:09:02 +0000

    Adding support for bhosphorus22k dataset

--- openhands/datasets/isolated/bhosporus22k.py
+++ openhands/datasets/isolated/bhosporus22k.py
@@ -6,7 +6,7 @@ from ..data_readers import load_frames_from_video
 
 class Bhosporus22kDataset(BaseIsolatedDataset):
     """
-    Turkis Isolated Sign language dataset(Bhosporus22k) from the paper:
+    Turkish Isolated Sign language dataset(Bhosporus22k) from the paper:
     Link to paper: https://arxiv.org/pdf/2004.01283.pdf
     """
     def read_glosses(self):
@@ -19,10 +19,10 @@ class Bhosporus22kDataset(BaseIsolatedDataset):
 
     def read_original_dataset(self):
         """
-        Dataset includes 3200 videos where 10 non-expert subjects executed 5 repetitions of 64 different types of signs.
+        Dataset includes 22542 videos where  6 signers executed 4+ repetitions of 744 different types of signs.
 
-        For train-set, we use signers 1-8.
-        Val-set & Test-set: Signer-9 & Signer-10
+        For train-set, we use all signers except signer called user_4. It contains 18,018 videos.
+        Test-set: Signer for test set is user_4, total number of videos 4525.
         
         """
 
@@ -37,7 +37,7 @@ class Bhosporus22kDataset(BaseIsolatedDataset):
             gloss_cat = self.label_encoder.transform([gloss.strip(' \n\t')])[0]
             if (
                 ((signer_id) !=4 and "train" in self.splits)
-                or (signer_id == 4 and "val" in self.splits)
+                or (signer_id == 4 and "test" in self.splits)
             ):
                 instance_entry = file_name, gloss_cat
                 #print(instance_entry)
