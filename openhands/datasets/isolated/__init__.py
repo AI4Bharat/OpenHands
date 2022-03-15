a3c2c416395d70c7eb63294d955a84d1c8ea4410
Ubuntu <manideepladi@gmail.com>
2022-03-15 18:00:18 +0000

    Adding support for MSASL
    (cherry picked from commit 34b85f425b5b81eeef84a41382fd7694660b616e)

--- openhands/datasets/isolated/__init__.py
+++ openhands/datasets/isolated/__init__.py
@@ -5,5 +5,8 @@ from .gsl import GSLDataset
 from .include import INCLUDEDataset
 from .lsa64 import LSA64Dataset
 from .wlasl import WLASLDataset
+from .msasl import MSASLDataset
+from .bhosporus22k import Bhosporus22kDataset
+from .asllvd import ASLLVDDataset
 
-__all__ = ["AUTSLDataset", "CSLDataset", "DeviSignDataset", "GSLDataset", "INCLUDEDataset", "LSA64Dataset", "WLASLDataset"]
+__all__ = ["AUTSLDataset", "CSLDataset", "DeviSignDataset", "GSLDataset", "INCLUDEDataset", "LSA64Dataset", "WLASLDataset","MSASLDataset","Bhosporus22kDataset","ASLLVDDataset"]
